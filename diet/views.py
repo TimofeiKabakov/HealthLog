ef2f3f45e1f99222a63c29be76248e5a2e7cc6f9
Xianglong Du (xid379) <xid379@usask.ca>
2022-12-21 15:08:09 -0600

    rest_framework added; Food API implemented

--- diet/views.py
+++ diet/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import FoodSerializer
+from .models import Food
 
 # Create your views here.
+
+class FoodView(viewsets.ModelViewSet):
+    serializer_class = FoodSerializer
+    queryset = Food.objects.all()
