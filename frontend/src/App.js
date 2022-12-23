b95a34735418ccd7cdb8311ea6b43873713ff3ec
Xianglong Du (xid379) <xid379@usask.ca>
2022-12-22 19:00:56 -0600

    partial food search implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [results, setResults] = useState([]);
+
+  const searchFood = (query) => {
+    const API_ENDPOINT = "https://api.nal.usda.gov/fdc/v1/foods/search";
+    const API_KEY = "DgN17Iv5tFj5hYJgwzbHgK7mIPpEFTQqUQEgE3eS";
+
+    const params = {
+      api_key: API_KEY,
+      query,
+    };
+
+    const queryString = Object.entries(params)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&');
+    
+    const url = `${API_ENDPOINT}?${queryString}`;
+
+    return fetch(url).then((response) => response.json());
+  }
+  
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.query.value;
+    searchFood(query).then((results) => setResults(results));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSearch}>
+        <input type='text' name='query' />
+        <button type='submit'>Search</button>
+      </form>
+      {results.totalHits > 0 && (
+        <ul>
+          {results.foods.map((food) => (
+            <li key={food.fdcId}>{food.description}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
