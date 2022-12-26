da51ab8dbe5d69573ec203030d902bcf7e633b7d
Irtisam <irtisami@gmail.com>
2022-12-26 17:54:24 -0600

    initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# class Meals(models.Model):
+class Meals(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
