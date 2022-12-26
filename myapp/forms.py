8363c4cf210696309c015853755811a78d36e7a3
Timofei Kabakov <timofeikabakov22@gmail.com>
2022-12-26 17:14:15 -0600

    now only logged in users can view the home page, also added some comments to my code

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+# extends the UserCreationForm provided by Django and adds one additional field for email
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta:
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
+# make sure that we can only view the home page when logged in
+# if not, redirect to '/login' with the help of decorator
+@login_required(login_url="/login")
 def home(request):
     return render(request, 'myapp/home.html')
 
 def sign_up(request):
+    # create an empty form or fill it with data provided by the user and render it
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
+            # if all fields were filled correctly, redirect the registered user the home page
             return redirect('/home')
     else:
         form = RegisterForm()
