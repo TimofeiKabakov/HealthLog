b832c1956be01ebb283c45b124ab489a6e7946c3
Denys43 <78376426+Denys43@users.noreply.github.com>
2022-12-27 15:25:26 -0600

    Add files via upload

--- healthlog/settings.py
+++ healthlog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api.apps.ApiConfig",
+    "rest_framework",
+    "frontend.apps.FrontendConfig"
 ]
 
 MIDDLEWARE = [
