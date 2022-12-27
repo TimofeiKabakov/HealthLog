a206d46111eb18626cedfeb824f43a978b1579b6
Irtisam <irtisami@gmail.com>
2022-12-27 17:39:29 -0600

    finished Food and Meal model, some modifications may be needed

--- myapp/models.py
+++ myapp/models.py
@@ -3,8 +3,37 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 # class Meals(models.Model):
-class Meals(models.Model):
+
+"""
+Meal model which includes multiple foods (many to many)
+and various other fields
+"""
+class Meal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
     def __str__(self):
         return self.name
 
+"""
+Food model which can be part of many meals (many to many)
+has fields for various nutritional values
+"""
+class Food(models.Model):
+    user = models.ManyToManyField(User, related_name='user')
+    meal = models.ManyToManyField(Meal, related_name='meal')
+    name = models.CharField(max_length=200)
+    calories = models.PositiveIntegerField()
+    serving_amount = models.PositiveIntegerField()
+    serving_unit = models.CharField(max_length=50)
+    # Unit is grams
+    total_fat = models.PositiveIntegerField()
+    carbohydrates = models.PositiveIntegerField()
+    protein = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
