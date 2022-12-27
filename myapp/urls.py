f1b5bd6a7044e838f0a0011bee1d61539d513e74
Timofei Kabakov <timofeikabakov22@gmail.com>
2022-12-27 02:23:13 -0600

    small changes to html

--- myapp/urls.py
+++ myapp/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     # documentation: https://docs.djangoproject.com/en/3.2/topics/auth/default/
     path('password-reset', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
     path('password-reset-done', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('password-reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name="password_reset_confirm"),
     path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name="password_reset_complete"),
 ]
