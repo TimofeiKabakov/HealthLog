cf7ee207389b86f8fd2e76e768098c176bac2f74
Irtisam <irtisami@gmail.com>
2022-12-29 19:30:14 -0600

    merged to have workout and exercise models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Exercise(models.Model):
@@ -11,4 +12,38 @@ class Exercise(models.Model):
 class WorkOut(models.Model):
     name = models.CharField(max_length=40, default="")
     exercises = models.ManyToManyField(Exercise)
-    
+
+
+"""
+Meal model which includes multiple foods (many to many)
+and various other fields
+"""
+class Meal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+    def __str__(self):
+        return self.name
+
+"""
+Food model which can be part of many meals (many to many)
+has fields for various nutritional values
+"""
+class Food(models.Model):
+    user = models.ManyToManyField(User, related_name='user')
+    meal = models.ManyToManyField(Meal, related_name='meal')
+    name = models.CharField(max_length=200)
+    calories = models.PositiveIntegerField()
+    serving_amount = models.PositiveIntegerField()
+    serving_unit = models.CharField(max_length=50)
+    # Unit is grams
+    total_fat = models.PositiveIntegerField()
+    carbohydrates = models.PositiveIntegerField()
+    protein = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
