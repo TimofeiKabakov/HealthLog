bb555fbc88c6d676caf05497fe066748376e9ecc
Irtisam <irtisami@gmail.com>
2022-12-30 17:26:45 -0600

    made view and urls for food and meal

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ExerciseView, WorkOutView
+from .views import ExerciseView, WorkOutView, MealView, FoodView
 
 urlpatterns = [
     path("exercise", ExerciseView.as_view()),
     path("work-out", WorkOutView.as_view()),
+    path("make-meal", MealView.as_view()),
+    path("make-food", FoodView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import ExerciseSerializer, WorkOutSerializer
-from .models import Exercise, WorkOut
+from .serializers import ExerciseSerializer, WorkOutSerializer, MealSeralizer, FoodSerializer
+from .models import Exercise, WorkOut, Meal, Food
 
 # Create your views here.
 
@@ -12,3 +12,11 @@ class ExerciseView(generics.ListAPIView):
 class WorkOutView(generics.ListAPIView):
     queryset = WorkOut.objects.all()
     serializer_class = WorkOutSerializer
+
+class MealView(generics.CreateAPIView):
+    queryset = Meal.objects.all()
+    serializer_class = MealSeralizer
+
+class FoodView(generics.CreateAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
