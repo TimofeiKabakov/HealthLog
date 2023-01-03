54c250988ffedd6de1af0efd5dab16974791199c
Xianglong Du (xid379) <xid379@usask.ca>
2023-01-03 17:10:34 -0600

    forgot to merge DietPage and current_meals, added

--- frontend/src/pages/DietPage.js
+++ frontend/src/pages/DietPage.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Card } from "../components/Card/Card";
+import AddMeal from "../components/Card/AddNewCard";
+import DietPageCSS from "./DietPage.module.scss";
+import Sidebar from "../components/Sidebar/Sidebar";
 import AddMealModal from "../components/AddMealModal";
 
-const DietPage = () => {
+const DietPage = (props) => {
+  const [userMeals, setUserMeals] = useState();
+  const image = require("../assets/food.jpg");
+
+  async function getMealsFetch() {
+    //TODO: error handling
+    const response = await fetch("/meals");
+    const res = await response.json();
+    return res;
+  }
+
+  useEffect(() => {
+    async function getMeals() {
+      const meals = await getMealsFetch();
+      setUserMeals(() => meals);
+    }
+    getMeals();
+  }, []);
+
   return (
-    <div>
-      <h1>This is the home page of the Diet component.</h1>
-      <AddMealModal />
+    <div className={DietPageCSS.Dashboard}>
+      <div className={DietPageCSS.DashboardGlass}>
+        <Sidebar selectedIndex={2} />
+        <div className={DietPageCSS.DietPage}>
+          <h1>Here are your meals:</h1>
+          <div className={DietPageCSS.Cards}>
+            {userMeals ? (
+              userMeals.map((meal) => <Card name={meal.name} image={image} />)
+            ) : (
+              <p>You have not added any meals yet</p>
+            )}
+          </div>
+        </div>
+        <AddMealModal />
+      </div>
     </div>
   );
 };
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
 from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from diet.models import Meal
+from diet.serializers import MealSeralizer
 from .forms import SignupForm
 
 class SignUpView(CreateView):
@@ -15,3 +17,11 @@ def log_out(request):
     """ function based logout view """
     logout(request)
     return HttpResponseRedirect("/login")
+
+@api_view(["GET"])
+def current_meals(request):
+    user = request.user
+    if user.is_authenticated:
+        queryset = Meal.objects.filter(user=user)
+        serializer = MealSeralizer(queryset, many=True)
+        return Response(serializer.data)
