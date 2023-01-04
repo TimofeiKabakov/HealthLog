4615b7a215daf2e307e88bddff82b0853644b61a
Xianglong Du (xid379) <xid379@usask.ca>
2023-01-03 18:02:57 -0600

    erases state after close addMealModal

--- frontend/src/components/AddMealModal.js
+++ frontend/src/components/AddMealModal.js
@@ -132,6 +132,9 @@ const AddMealModal = () => {
           addFood(mealId, meal[i]);
         }
       });
+      setSearchResults();
+      setCurrentFood();
+      setMeal([]);
       closeModal();
     }
   };
