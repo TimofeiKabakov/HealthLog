fd0aeeae0d5cce922c14ce279947c85553514a12
Timofei Kabakov <120842194+TimofeiKabakov@users.noreply.github.com>
2023-01-28 23:54:01 -0600

    Update settings.py

--- healthlog/settings.py
+++ healthlog/settings.py
@@ -129,5 +129,3 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "lamename24@gmail.com"
-EMAIL_HOST_PASSWORD = "fbtpgiqpdphuxphz"
